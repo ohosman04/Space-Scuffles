b7e91bd857010e0776540bc4612383025d4c37cc
ohosman04 <105369697+ohosman04@users.noreply.github.com>
2023-02-04 14:52:07 -0500

    Update main.py

--- Space Scuffles/main.py
+++ Space Scuffles/main.py
@@ -136,8 +136,8 @@ def confrontation():
                     run = False
                     pygame.quit()
 
-                if event.type == pygame.KEYDOWN and len(yellow_bullets) < MAX_BULLETS:
-                    if event.key == pygame.K_LCTRL:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
                         bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height// 2 - 2,10,5)
                         yellow_bullets.append(bullet)
                         BULLET_FIRE_SOUND.play()
